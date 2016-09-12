6f53717cf829392230c92b0debab800fdc509f38
Juan Carlos Valdés <jcvaldes@combinatoria.com.ar>
2016-09-11 23:18:39 -0300

    Cambiando el idioma y almacenándolo en localStorage

--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -2,5 +2,20 @@ module.exports = {
     likes: '{ likes, plural, ' +
             '=0 { no likes }' +
             '=1 { # like }' +
-            'other { # likes }}'
+            'other { # likes }}',
+    'logout': 'Logout',
+    'english': 'English',
+    'spanish': 'Spanish',
+    'signup.subheading': 'Signup to watch your friend\'s picture studying at Platzi',
+    'signup.facebook': 'Signup with Facebook',
+    'signup.text': 'Signup',
+    'email': 'Email',
+    'username': 'Username',
+    'fullname': 'Fullname',
+    'password': 'Password',
+    'signup.call-to-action': 'Signup',
+    'signup.have-account': 'Alredy have an account?',
+    'signin': 'Signin',
+    'signin.not-have-account': 'Don\'t have an account?',
+    'language': 'Language'
 }
