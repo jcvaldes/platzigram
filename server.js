e2a0b4b671d410fb00f1bcb8a6d1e5ec55e920df
Juan Carlos Valdés <jcvaldes@combinatoria.com.ar>
2016-09-18 17:48:03 -0300

    Subiendo la foto a nuestro servidor web

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 var express = require('express');
+var multer = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        //cb(null, file.fieldname + '-' + Date.now())
+        cb(null, +Date.now() + '.' + ext(file.originalname));
+    }
+})
+
+var upload = multer({ storage: storage }).single('picture'); //picture es el pàrametro
+
 var app = express();
 
 app.set('view engine', 'pug');
@@ -49,6 +64,15 @@ app.get('/api/pictures', function(req, res, next) {
     setTimeout(function () {
         res.send(pictures);
     }, 2000);
+});
+
+app.post('/api/pictures', function (req, res) {
+    upload(req, res, function (err) {
+        if(err) {
+            return res.send(500, "Error uploading file");
+        }
+        return res.send("File uploaded successfully");
+    })
 });
 
 app.listen(3000, function(err){
